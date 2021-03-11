54060974c2b9665413f4703b2c8132f727edeadd
TheKingElessar <wasabimath@gmail.com>
2021-03-11 14:54:37 -0700

    k

--- module_confessions.py
+++ module_confessions.py
@@ -35,9 +35,9 @@ class ListenerConfession(commands.Cog):
             if len(embed_list) > 0:
                 embed_url_list = []
                 for embed in embed_list:
-                    if embed.image != Embed.Empty:
+                    if str(embed.image) != "Embed.Empty":
                         embed_url_list.append(embed.image.url)
-                    if embed.video != Embed.Empty:
+                    if str(embed.video) != "Embed.Empty":
                         embed_url_list.append(embed.video.url)
                 print(embed_url_list)
                 serialized_embeds = json.dumps(embed_url_list)
